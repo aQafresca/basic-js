842e4525659386ccd9cbbb3c348b2cd8a7a69ab0
ill_Nino <ill_Nino@bk.ru>
2024-12-19 20:40:43 +0300

    feat: add solution part4

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  const catsEars = '^^';
+  let result = 0;
+
+for (let i = 0; i < matrix.length; i += 1) {
+  const row = matrix[i];
+  row.forEach(element => {
+    if (element === catsEars) {
+      result += 1;
+    }
+  });
+}
+return result;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+let newArr = [];
+if (members === null || members === undefined) return false;
+
+for (let i = 0; i < members.length; i += 1) {
+  const char = members[i];
+  if (typeof char === 'string') {
+    const firstLetter = char.trim().charAt(0).toUpperCase();
+    newArr.push(firstLetter);
+  }
+}
+return newArr.sort().join('');
 }
 
 module.exports = {
